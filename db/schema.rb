c3cb6fd3e700324cd92f9a1c6ad4fb6383fe4dfb
th0r0nd0r <pjfritch1@gmail.com>
2018-01-17 20:29:22 -0800

    create conversions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180118042318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversions", force: :cascade do |t|
+    t.string "freedom_unit"
+    t.string "si_unit"
+    t.float "conversion_factor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
