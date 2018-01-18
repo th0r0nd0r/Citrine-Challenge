794bfe8e853277616185e001797a91cf508fc6e6
th0r0nd0r <pjfritch1@gmail.com>
2018-01-18 09:39:48 -0800

    add comments

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -24,26 +24,24 @@ class Unit < ApplicationRecord
       "t": "1000.0"
     }
 
+    # sample values for 'units' argument
     # "degree/minute"
     # "(degree(minute/hectare))"
 
-    # solving strategy:
-    # create unit hash and value hash
-    # split the units query on ( / * and ) with regex
-    # split the inverse of the query, creating array with operators
-    # use hashes to map input units to an array of si units and an array of values
-    # use a conditional statment to figure out whether the first character in query was an operator or unit
-    # zip both new arrays with the array of operators, and flatten
-    # join both, call eval method on value string to get the multiplication factor
 
+    # splitting the input into an array of units and an array of operators
     freedom_units = units.split(/[()*\/]/)
     operators = units.split(/[^()*\/]/)
 
+    # creating arrays for si units and values
     si_units = freedom_units.map { |unit| unit_conversions[unit] }
     converted_values = freedom_units.map { |unit| value_conversions[unit] }
 
+    # figuring out whether to start zipping with units or operators
     first_char = units[0]
 
+    # zipping units and values up with operators, and converting to final data types
+    # string and float
     if operators.include?(first_char)
       unit_name = operators.zip(si_units).flatten.join
       m_f = eval(operators.zip(converted_values).flatten.join)
@@ -52,6 +50,7 @@ class Unit < ApplicationRecord
       m_f = eval(converted_values.zip(operators).flatten.join)
     end)
 
+    # rounding the multiplication factor to 14 decimal places
     multiplication_factor = Integer(m_f * (10**14)) / Float(10**14)
 
 
