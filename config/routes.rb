a5ddbac9340b8400fb648e659d48e5fa353dbbd2
th0r0nd0r <pjfritch1@gmail.com>
2018-01-17 20:23:26 -0800

    generate conversion model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :units, only: [:show], defaults: {format: json}
+  resources :units, only: [:show], defaults: {format: :json}
 end
