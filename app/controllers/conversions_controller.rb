a505dc5cafb68370dd5ad41753b6ada614361e9f
th0r0nd0r <pjfritch1@gmail.com>
2018-01-17 20:31:08 -0800

    create conversions controller

---
+++ app/controllers/conversions_controller.rb
@@ -0,0 +1,3 @@
+class ConversionsController < ApplicationController
+  
+end
---
+++ test/controllers/conversions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConversionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
